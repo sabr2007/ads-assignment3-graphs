988e73302bec9c0dfed5b7c58100e0e7cec6d1d2
Sabyrzhan <sabyrzanvaliullov@gmail.com>
2026-05-21 22:28:44 +0500

    feat(edge) add weight field for dijkstra bonus

--- src/Edge.java
+++ src/Edge.java
@@ -2,10 +2,16 @@ public class Edge{
 
     private Vertex source;
     private Vertex destination;
+    private int weight;
 
     public Edge(Vertex source, Vertex destination){
-        this.destination = destination;
+        this(source, destination, 1);
+    }
+
+    public Edge(Vertex source, Vertex destination, int weight){
         this.source = source;
+        this.destination = destination;
+        this.weight = weight;
     }
 
     public Vertex getSource() {
@@ -16,8 +22,12 @@ public class Edge{
         return destination;
     }
 
+    public int getWeight() {
+        return weight;
+    }
+
     @Override
     public String toString(){
-        return source + " -> " + destination;
+        return source + " -> " + destination + " (w=" + weight + ")";
     }
 }
