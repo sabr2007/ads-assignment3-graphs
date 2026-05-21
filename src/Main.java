31ebc40afd5b5612856c0271545b0f171e841dab
Sabyrzhan <sabyrzanvaliullov@gmail.com>
2026-05-21 22:30:30 +0500

    feat(main) add weighted demo graph and dijkstra run

--- src/Main.java
+++ src/Main.java
@@ -20,6 +20,23 @@ public class Main {
         demo.bfs(0);
         demo.dfs(0);
 
+        System.out.println("\nweighted demo graph:");
+        Graph weighted = new Graph();
+        for (int i = 0; i < 6; i++) {
+            weighted.addVertex(new Vertex(i));
+        }
+
+        weighted.addEdge(0, 1, 4);
+        weighted.addEdge(0, 2, 1);
+        weighted.addEdge(2, 1, 2);
+        weighted.addEdge(1, 3, 1);
+        weighted.addEdge(2, 3, 5);
+        weighted.addEdge(3, 4, 3);
+        weighted.addEdge(4, 5, 2);
+
+        weighted.printWeightedGraph();
+        weighted.dijkstra(0);
+
         Experiment exp = new Experiment();
         exp.runMultipleTests();
         exp.printResults();
